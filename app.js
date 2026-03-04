de48d09daa557e220c5d130d3c8a9896d67bbc4d
Paban-1 <rpaban89@gmail.com>
2026-03-04 20:28:14 +0530

    Start Server & connect MongoDB with page rendaring

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 
 const userModel = require('./models/user')
 
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 app.get('/', (req, res) => {
-    res.send('hi')
+    res.render('index')
+})
+
+app.post('/register', async (req, res) => {
+    const { username, name, age, email, password } = req.body
 })
 
 app.listen(3000, () => {
