a5cd0a246c77904155ba6b9d653e668bee334e25
Paban-1 <rpaban89@gmail.com>
2026-03-04 21:01:57 +0530

    Creating and testing /register route with bcrypt

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
 
 const userModel = require('./models/user')
 
@@ -16,6 +17,16 @@ app.get('/', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { username, name, age, email, password } = req.body
+    let user = await userModel.findOne({ email })
+    if (user) return res.status(500).send("User already exists")
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+          
+
+        })
+
+    })
 })
 
 app.listen(3000, () => {
